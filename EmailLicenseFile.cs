e27853108f92a56b9764779c005ef27692a00680
Javier Ramos Nodal <javiramos@soliss.com>
2023-02-07 20:40:33 +0100

    SendGridMessage

--- EmailLicenseFile.cs
+++ EmailLicenseFile.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using SendGrid.Helpers.Mail;
 
 namespace pluralsight_azurefunctions
 {
@@ -11,12 +12,21 @@ namespace pluralsight_azurefunctions
     {
         [FunctionName("EmailLicenseFile")]
         public void Run(
-            [BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")]string licenseFileContent, 
+            [BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")]string licenseFileContent,
+            [SendGrid(ApiKey = "SendGridApiKey")] out SendGridMessage message,
             string name, 
             ILogger log)
         {
             var email = Regex.Match(licenseFileContent, @"^Email\:\ (.+)$", RegexOptions.Multiline).Groups[1].Value;
             log.LogInformation($"Got order from {email}\n License file Name:{name}");
+            message = new SendGridMessage();
+            message.From = new EmailAddress(Environment.GetEnvironmentVariable("EmailSender"));
+            message.AddTo(email);
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(licenseFileContent);
+            var base64 = Convert.ToBase64String(plainTextBytes);
+            message.AddAttachment(name, base64, "text/plain");
+            message.Subject = "Your license file";
+            message.HtmlContent = "Thank you for your order";
         }
     }
 }
