f534952818895ed1781dfdee654ea9d3a9580d96
Javier Ramos Nodal <javiramos@soliss.com>
2023-02-09 22:35:35 +0100

    IBinder and Table Storage Input.

--- EmailLicenseFile.cs
+++ EmailLicenseFile.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using pluralsight_azurefunctions.Models;
 using SendGrid.Helpers.Mail;
 
 namespace pluralsight_azurefunctions
@@ -12,21 +13,44 @@ namespace pluralsight_azurefunctions
     {
         [FunctionName("EmailLicenseFile")]
         public void Run(
-            [BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")]string licenseFileContent,
-            [SendGrid(ApiKey = "SendGridApiKey")] out SendGridMessage message,
-            string name, 
+            [BlobTrigger("licenses/{orderId}.lic", Connection = "AzureWebJobsStorage")] string licenseFileContent,
+            [SendGrid(ApiKey = "SendGridApiKey")] ICollector<SendGridMessage> sender,
+            [Table("orders", "orders", "{orderId}")] Order order,
+            string orderId,
             ILogger log)
         {
-            var email = Regex.Match(licenseFileContent, @"^Email\:\ (.+)$", RegexOptions.Multiline).Groups[1].Value;
-            log.LogInformation($"Got order from {email}\n License file Name:{name}");
-            message = new SendGridMessage();
+            var email = order.Email;
+            log.LogInformation($"Got order from {email}\n Order Id:{$"{orderId}"}");
+            var message = new SendGridMessage();
             message.From = new EmailAddress(Environment.GetEnvironmentVariable("EmailSender"));
             message.AddTo(email);
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(licenseFileContent);
             var base64 = Convert.ToBase64String(plainTextBytes);
-            message.AddAttachment(name, base64, "text/plain");
+            message.AddAttachment($"{orderId}.lic", base64, "text/plain");
             message.Subject = "Your license file";
             message.HtmlContent = "Thank you for your order";
+
+            if (!email.EndsWith("@test.com"))
+            {
+                sender.Add(message);
+            }
         }
+        //public void Run(
+        //    [BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")]string licenseFileContent,
+        //    [SendGrid(ApiKey = "SendGridApiKey")] out SendGridMessage message,
+        //    string name, 
+        //    ILogger log)
+        //{
+        //    var email = Regex.Match(licenseFileContent, @"^Email\:\ (.+)$", RegexOptions.Multiline).Groups[1].Value;
+        //    log.LogInformation($"Got order from {email}\n License file Name:{name}");
+        //    message = new SendGridMessage();
+        //    message.From = new EmailAddress(Environment.GetEnvironmentVariable("EmailSender"));
+        //    message.AddTo(email);
+        //    var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(licenseFileContent);
+        //    var base64 = Convert.ToBase64String(plainTextBytes);
+        //    message.AddAttachment(name, base64, "text/plain");
+        //    message.Subject = "Your license file";
+        //    message.HtmlContent = "Thank you for your order";
+        //}
     }
 }
--- GenerateLicenseFile.cs
+++ GenerateLicenseFile.cs
@@ -3,17 +3,24 @@ using Microsoft.Extensions.Logging;
 using pluralsight_azurefunctions.Models;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace pluralsight_azurefunctions
 {
     public class GenerateLicenseFile
     {
         [FunctionName("GenerateLicenseFile")]
-        public void Run(
-            [QueueTrigger("orders", Connection = "AzureWebJobsStorage")]Order order,
-            [Blob("licenses/{rand-guid}.lic")] TextWriter outputBlob,
+        public async Task Run(
+            [QueueTrigger("orders", Connection = "AzureWebJobsStorage")] Order order,
+            IBinder binder,
             ILogger log)
         {
+            var outputBlob = await binder.BindAsync<TextWriter>(
+                new BlobAttribute($"licenses/{order.OrderId}.lic")
+                {
+                    Connection= "AzureWebJobsStorage"
+                });
+
             outputBlob.WriteLine($"OrderId: {order.OrderId}");
             outputBlob.WriteLine($"Email: {order.Email}");
             outputBlob.WriteLine($"ProductId: {order.ProductId}");
@@ -23,5 +30,19 @@ namespace pluralsight_azurefunctions
             outputBlob.WriteLine($"SecretCode: {BitConverter.ToString(hash).Replace("-", "")}");
             log.LogInformation($"C# Queue trigger function processed: {order}");
         }
+        //public void Run(
+        //    [QueueTrigger("orders", Connection = "AzureWebJobsStorage")]Order order,
+        //    [Blob("licenses/{rand-guid}.lic")] TextWriter outputBlob,
+        //    ILogger log)
+        //{
+        //    outputBlob.WriteLine($"OrderId: {order.OrderId}");
+        //    outputBlob.WriteLine($"Email: {order.Email}");
+        //    outputBlob.WriteLine($"ProductId: {order.ProductId}");
+        //    outputBlob.WriteLine($"PurchaseDate: {DateTime.UtcNow}");
+        //    var md5 = System.Security.Cryptography.MD5.Create();
+        //    var hash = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(order.Email + "secret"));
+        //    outputBlob.WriteLine($"SecretCode: {BitConverter.ToString(hash).Replace("-", "")}");
+        //    log.LogInformation($"C# Queue trigger function processed: {order}");
+        //}
     }
 }
